b37d5c14019e9fd82f9f6a102d06feaecaaf6f33
Prashontya Kar <karprashontya@gmail.com>
2020-08-08 17:24:52 +0530

    add gym_equipment_app on our main project

---
+++ gym_equipment_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GymEquipmentAppConfig(AppConfig):
+    name = 'gym_equipment_app'
--- project_physitude/settings.py
+++ project_physitude/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gym_equipment_app',
 ]
 
 MIDDLEWARE = [
